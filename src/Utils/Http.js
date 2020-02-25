80aee96211f377223cd8654dccec4f6168b33980
larrybuntus <larrybuntus@gmail.com>
2020-02-25 10:09:44 +0000

    updated to make use of localforage and axios-cache-adapter

--- src/Utils/Http.js
+++ src/Utils/Http.js
@@ -1,64 +1,105 @@
-import React from "react";
+import React, { Fragment } from "react";
 /**
  * packages
  */
-import Axios from "axios";
+import localforage from "localforage";
+import { setup } from "axios-cache-adapter";
 import { toast } from "react-toastify";
-/**
- * utilities
- */
-import Notify from "./Notify";
 
-export default function Http({
-  url,
-  type = "get",
-  data = {},
-  application = "application/json"
-}) {
-  let response = null;
-  const axiosInstance = Axios.create({
-    baseURL: process.env.REACT_APP_API,
-    timeout: 180000,
-    headers: {
-      Authorization: `Bearer ${
-        localStorage["access_token"] ? localStorage["access_token"] : ""
-      }`,
-      "Content-Type": application,
-      Accept: "application/json"
-    }
-  });
+export const Http = setup({
+  // timeout: 1000,
+  baseURL: process.env.REACT_APP_API,
+  headers: {
+    // "X-CSRF-TOKEN": token?.content,
+    "Content-Type": "application/json",
+    "X-Requested-With": "XMLHttpRequest",
+    Accept: "application/json",
+    request_client: "browser"
+  },
+  cache: {
+    maxAge: 15 * 60 * 1000,
 
-  switch (type) {
-    case "get":
-      response = axiosInstance.get(url, { params: data });
-      break;
-    case "post":
-      response = axiosInstance.post(url, data);
-      break;
-    case "put":
-      response = axiosInstance.put(url, data);
-      break;
-    case "delete":
-      response = axiosInstance.delete(url, data);
-      break;
-    default:
-      break;
-  }
+    store: localforage.createInstance({
+      // List of drivers used
+      driver: [localforage.INDEXEDDB, localforage.LOCALSTORAGE],
+      // Prefix all storage keys to prevent conflicts
+      name: "your-app-uniques"
+    }),
 
-  if (response) {
-    response.catch(error => {
-      if (error.response) {
-        if (error.response.status === 401) {
-          delete localStorage["user"];
-          delete localStorage["jwt"];
-          window.location.reload();
-        }
-      } else {
-        toast(<Notify body="A server error occured." type="error" />);
+    // Invalidate only when a specific option is passed through config
+    invalidate: async (config, request) => {
+      if (config.clearAll) {
+        await config.store.clear();
       }
-    });
-  } else {
+
+      if (request.crearEntry) {
+        await config.store.removeItem(config.uuid);
+      }
+
+      if (request.method.toLowerCase() !== "get") {
+        config.uuid.match(/.+\/v5\/\w+(\/w+)?/).forEach(async v => {
+          await config.store.removeItem(v);
+        });
+        await config.store.removeItem(config.uuid.match(/.+\/v5\/\w+/)[0]);
+      }
+    },
+
+    // {Object} Define which kind of requests should be excluded from cache.
+    exclude: {
+      // {Array} List of regular expressions to match against request URLs.
+      paths: [/\/auth\/.+/],
+      // {Boolean} Exclude requests with query parameters.
+      query: true,
+      // {Function} Method which returns a `Boolean` to determine if request
+      // should be excluded from cache.
+      filter: null
+    },
+
+    debug: process.env.REACT_APP_ENV === "dev"
+  }
+});
+
+Http.interceptors.request.use(config => {
+  if (!/(\/auth\/(?!refresh|logout|me|email).+)/.test(config.url)) {
+    config.headers["Authorization"] = `Bearer ${localStorage["jwt_token"]}`;
   }
 
-  return response;
-}
+  return config;
+});
+
+Http.interceptors.response.use(
+  response => response.data,
+  error => {
+    if (error.response?.status) {
+      if (error.response.status === 401) return false;
+
+      if (error.response.status !== 422)
+        toast.error(
+          error.response?.data?.error?.message ||
+            (error.response?.data).message ||
+            error.message
+        );
+
+      if (process.env.REACT_APP_ENV === "dev") {
+        toast.info(
+          <Fragment>
+            <p className="error-title">Http Exception</p>
+            <hr />
+            <div className="error-content">
+              <pre>{JSON.stringify(error, 2, 2)}</pre>
+            </div>
+          </Fragment>,
+          {
+            rtl: true,
+            autoClose: 30000,
+            toastId: "dev-error-toast",
+            className: "dev-error-toast",
+            position: toast.POSITION.BOTTOM_LEFT
+          }
+        );
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
